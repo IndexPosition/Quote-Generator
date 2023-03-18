39e734c026b2ba3c95a68bfc2e1e2679b1cc6f42
Abhijith Prakash <99633082+IndexPosition@users.noreply.github.com>
2023-03-19 03:33:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const quoteText = document.querySelector("#quote-text");
+const quoteAuthor = document.querySelector("#quote-author");
+const newQuoteBtn = document.querySelector("#new-quote");
+
+function getQuote() {
+  fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((data) => {
+      quoteText.textContent = `"${data.content}"`;
+      quoteAuthor.textContent = `- ${data.author}`;
+    })
+    .catch((err) => console.log(err));
+}
+
+newQuoteBtn.addEventListener("click", getQuote);
+getQuote();
